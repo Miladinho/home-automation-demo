b66cf32b569f6d3251917ffa0a235aed5d768159
Milad Gueramian <miladgueramian@gmail.com>
2020-09-24 15:41:53 -0700

    new test

--- home.py
+++ home.py
@@ -3,12 +3,15 @@ class Home:
         self.data = {}
     
     def add(self, component):
-        if not self.isConnected(component):
+        if not self.isConnected(component.name):
             self.data[component.name] = component
         else:
             raise Exception("Component Already Connected")
 
-    def isConnected(self, component) -> bool:
-        if component.name in self.data:
+    def remove(self, componentName):
+        return self.data.pop(componentName)
+        
+    def isConnected(self, componentName) -> bool:
+        if componentName in self.data:
             return True
         return False
--- lights-tests.py
+++ lights-tests.py
@@ -3,19 +3,26 @@ from light import Light
 from home import Home
 
 class LightTests(unittest.TestCase):
+    def setUp(self):
+        self.home = Home()
+
     def test_add_new_light(self):
         newLight = Light("test")
-        home = Home()
-        home.add(newLight)
-        self.assertTrue(home.isConnected(newLight))
+        self.home.add(newLight)
+        self.assertTrue(self.home.isConnected(newLight.name))
 
     def test_add_existing_light_throws(self):
         light = Light("test")
-        home = Home()
-        home.add(light)
-        self.assertRaises(Exception, home.add, **{"component": light})
+        self.home.add(light)
+        self.assertRaises(Exception, self.home.add, **{"component": light})
     
-
+    def test_remove_existing_light(self):
+        light = Light("test")
+        self.home.add(light)
+        self.assertTrue(len(self.home.data) == 1)
+        self.assertTrue(self.home.remove(light.name) == light)
+        self.assertTrue(len(self.home.data) == 0)
+    
         
 if __name__ == '__main__':
     unittest.main()
