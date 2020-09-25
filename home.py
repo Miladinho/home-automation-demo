f23ed781373dbf0ed065e290f5b24294e552955b
Milad Gueramian <miladgueramian@gmail.com>
2020-09-24 21:05:34 -0700

    refactoring

--- home.py
+++ home.py
@@ -14,12 +14,18 @@ class Home:
         if self.isConnected(componentName):
             return self.data.pop(componentName)
         raise ComponentNotConnectedError(componentName)
+
+    def get(self, componentName):
+        if self.isConnected(componentName):
+            return self.data[componentName]
+
+    def update(self, component):
+        if self.isConnected(component.name):
+            self.data[component.name] = component
+        else:
+            raise ComponentNotConnectedError(component.name)
         
     def isConnected(self, componentName):
         if componentName in self.data:
             return True
         return False
-
-    def get(self, componentName):
-        if self.isConnected(componentName):
-            return self.data[componentName]
--- lights-tests.py
+++ lights-tests.py
@@ -6,40 +6,40 @@ from errors import *
 class LightTests(unittest.TestCase):
     def setUp(self):
         self.home = Home()
-
-    def test_add_new_light(self):
         newLight = Light("test")
         self.home.add(newLight)
-        self.assertTrue(self.home.isConnected(newLight.name))
+
+    def test_add_new_light(self):
+        self.assertTrue(self.home.isConnected("test"))
 
     def test_add_existing_light_throws(self):
         light = Light("test")
-        self.home.add(light)
         self.assertRaises(ComponentAlreadyConnectedError, self.home.add, **{"component": light})
     
     def test_remove_existing_light(self):
-        light = Light("test")
-        self.home.add(light)
         self.assertTrue(len(self.home.data) == 1)
-        self.assertTrue(self.home.remove(light.name) == light)
+        light = self.home.remove("test")
+        self.assertTrue(light.name == "test")
         self.assertTrue(len(self.home.data) == 0)
     
     def test_remove_non_existing_light_throws(self):
+        self.home.data.clear()
         self.assertRaises(ComponentNotConnectedError, self.home.remove, **{"componentName": "test"})
     
     def test_turn_light_on(self):
-        light = Light("test")
-        self.home.add(light)
-        self.assertEqual(light.status, False)
-        self.home.get("test").status = True
-        self.assertEqual(light.status, True)
+        light = self.home.get("test")
+        self.assertEqual(light.status, False) # default status
+        light.status = True
+        self.home.update(light)
+        self.assertEqual(self.home.get("test").status, True)
     
     def test_turn_light_off(self):
-        light = Light("test", status=True)
-        self.home.add(light)
-        self.assertEqual(light.status, True)
-        self.home.get("test").status = False
-        self.assertEqual(light.status, False)
+        self.home.data.clear()
+        self.home.add(Light("test", status=True))
+        light = self.home.get("test")
+        light.status = False
+        self.home.update(light)
+        self.assertEqual(self.home.get("test").status, False)
         
 if __name__ == '__main__':
     unittest.main()
