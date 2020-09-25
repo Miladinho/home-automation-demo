4d71d43c1a907b5d7bcfa0837e0d5fed6cbae384
Milad Gueramian <miladgueramian@gmail.com>
2020-09-24 21:14:56 -0700

    thermostat adn tests

---
+++ thermostat.py
@@ -0,0 +1,4 @@
+class Thermostat:
+    def __init__(self, name, temp=72):
+        self.name = name
+        self.temperature = temp
---
+++ theromostat-tests.py
@@ -0,0 +1,19 @@
+import unittest
+from home import Home
+from thermostat import Thermostat
+
+class ThermostatTests(unittest.TestCase):
+    def setUp(self):
+        self.home = Home()
+        self.home.add(Thermostat("thermostat", temp=68))
+    
+    def test_read_temperature(self):
+        self.assertEqual(self.home.get("thermostat").temperature, 68)
+    
+    def test_set_temperature(self):
+        thermostat = self.home.get("thermostat")
+        thermostat.temperature = 75
+        self.assertEqual(self.home.get("thermostat").temperature, 75)
+
+if __name__ == '__main__':
+    unittest.main()
