fbd20d961be3b8d3c7f98078219aff3c63af8312
Milad Gueramian <miladgueramian@gmail.com>
2020-09-27 16:41:06 -0700

    update error internals

--- app/errors.py
+++ app/errors.py
@@ -1,7 +1,15 @@
 class ComponentNotConnectedError(Exception):
     def __init__(self, componentName):
-        super().__init__(f"Component with name \"{componentName}\" is not connected.")
+        message = "Component with name "+"\""+componentName+"\""+" is not connected."
+        self.message = message
+        super().__init__(message)
+    def __str__(self):
+        return "ComponentNotConnectedError: {0}".format(self.message)
 
 class ComponentAlreadyConnectedError(Exception):
     def __init__(self, componentName):
-        super().__init__(f"Component with name \"{componentName}\" is already connected.")
+        message = "Component with name "+"\""+componentName+"\""+" is already connected."
+        self.message = message
+        super().__init__(message)
+    def __str__(self):
+        return "ComponentAlreadyConnectedError: {0}".format(self.message)
