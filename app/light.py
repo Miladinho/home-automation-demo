ea992a3b51a0671564ee8b5bea35cb0044f2ceb3
Milad Gueramian <miladgueramian@gmail.com>
2020-09-27 16:40:27 -0700

    added type attribute

--- app/light.py
+++ app/light.py
@@ -1,4 +1,5 @@
 class Light:
     def __init__(self, name, status=0):
+        self.type = "Light"
         self.name = name
         self.status = status
--- app/thermostat.py
+++ app/thermostat.py
@@ -1,4 +1,5 @@
 class Thermostat:
     def __init__(self, name, temp=72):
+        self.type = "Thermostat"
         self.name = name
         self.temperature = temp
