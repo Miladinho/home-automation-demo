89bd9e1dc718395c5799867b430896f57fa9e1cd
Milad Gueramian <miladgueramian@gmail.com>
2020-09-29 01:59:05 -0700

    fixed bug with creating env file

--- web/setup.sh
+++ web/setup.sh
@@ -4,6 +4,5 @@
 npm install 
 
 # Setup environment variables file
-mkdir .env.development
 echo "GATSBY_API_URL=http://127.0.0.1:5000/api" > .env.development
 # Add .env.production if planning to run in production
